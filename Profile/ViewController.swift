b50cbe65264940258208d1cb06d2f5cc9a5ba737
Valeriya Khristinina <valeriya.khristinina@gmail.com>
2020-02-10 16:13:15 +0100

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Valeria Khristinina on 10/02/2020.
+//  Copyright © 2020 Valeria Khristinina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
